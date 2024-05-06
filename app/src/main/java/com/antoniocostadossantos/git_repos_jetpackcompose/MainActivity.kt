eab5fe6d12c170417ed7f5cab291a9aa6ad932bd
Antonio <antonio195br@gmail.com>
2024-05-06 11:42:32 -0300

    Projeto base

---
+++ app/src/main/java/com/antoniocostadossantos/git_repos_jetpackcompose/MainActivity.kt
@@ -0,0 +1,46 @@
+package com.antoniocostadossantos.git_repos_jetpackcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.antoniocostadossantos.git_repos_jetpackcompose.ui.theme.GitreposjetpackcomposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            GitreposjetpackcomposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    GitreposjetpackcomposeTheme {
+        Greeting("Android")
+    }
+}
