b6919fd22426dcab400eee501924bd507743ce76
Antonio <antonio195br@gmail.com>
2024-05-06 21:42:23 -0300

    Loading state

--- app/src/main/java/com/antoniocostadossantos/git_repos_jetpackcompose/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/antoniocostadossantos/git_repos_jetpackcompose/ui/screens/HomeScreen.kt
@@ -4,21 +4,20 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.LinearProgressIndicator
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
-import androidx.compose.runtime.setValue
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.antoniocostadossantos.git_repos_jetpackcompose.ui.components.RepositoryItem
 import com.antoniocostadossantos.git_repos_jetpackcompose.ui.components.SearchBar
+import kotlinx.coroutines.launch
 import org.koin.androidx.compose.koinViewModel
 
 @Composable
@@ -26,18 +25,27 @@ fun HomeScreen() {
 
     val viewModel = koinViewModel<HomeViewModel>()
     val uiState by viewModel.uiState.collectAsState()
-    var showProgress by remember { mutableStateOf(false) }
+    val scope = rememberCoroutineScope()
 
     Column(
         modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.SpaceBetween
     ) {
         SearchBar(
             text = uiState.lang,
             onSearchChange = { uiState.onSearchChange(it) },
-            onClickSearch = { viewModel.fetchData() }
+            onClickSearch = {
+                scope.launch {
+                    viewModel.fetchData()
+                }
+            }
         )
         LazyColumn(
             verticalArrangement = Arrangement.spacedBy(8.dp),
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f)
         ) {
             items(
                 items = uiState.repositories,
@@ -46,19 +54,8 @@ fun HomeScreen() {
                 RepositoryItem(item = it)
             }
         }
-        LinearProgressIndicator(
-            modifier = Modifier
-                .heightIn(min = 1.dp)
-                .fillMaxWidth()
-        )
-        when (uiState.loading) {
-            API.LOADING -> {
-                showProgress = true
-            }
-
-            else -> {
-                showProgress = false
-            }
+        if (uiState.showProgress) {
+            LinearProgressIndicator(modifier = Modifier.fillMaxWidth())
         }
     }
 }
--- app/src/main/java/com/antoniocostadossantos/git_repos_jetpackcompose/ui/screens/HomeViewModel.kt
+++ app/src/main/java/com/antoniocostadossantos/git_repos_jetpackcompose/ui/screens/HomeViewModel.kt
@@ -1,28 +1,19 @@
 package com.antoniocostadossantos.git_repos_jetpackcompose.ui.screens
 
-import android.util.Log
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.antoniocostadossantos.git_repos_jetpackcompose.data.GitHubRestApi
 import com.antoniocostadossantos.git_repos_jetpackcompose.model.Item
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.asStateFlow
 import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 
-enum class API {
-    LOADING,
-    ERROR,
-    SUCCESS,
-    STOPPED,
-}
-
 data class HomeUiState(
     val repositories: List<Item> = emptyList(),
     val lang: String = "kotlin",
     val onSearchChange: (String) -> Unit = {},
-    val loading: API = API.STOPPED,
+    val showProgress: Boolean = false
 )
 
 class HomeViewModel(
@@ -48,27 +39,17 @@ class HomeViewModel(
         }
     }
 
-    fun fetchData() {
-        viewModelScope.launch {
-            changeStateLoading(API.LOADING)
-
-            _uiState.update { currentState ->
-                currentState.copy(
-                    repositories = try {
-                        changeStateLoading(API.SUCCESS)
-                        api.getRepositories(_uiState.value.lang).items
-                    } catch (e: Exception) {
-                        Log.i("TESTE", e.toString())
-                        changeStateLoading(API.ERROR)
-                        emptyList()
-                    }
-                )
-            }
+    suspend fun fetchData() {
+        _uiState.update { currentState ->
+            currentState.copy(showProgress = true)
         }
-    }
-
-    private fun changeStateLoading(actualState: API) {
-        _uiState.update { currentState -> currentState.copy(loading = actualState) }
+        val repositories = api.getRepositories(_uiState.value.lang).items
+        _uiState.update { currentState ->
+            currentState.copy(
+                repositories = repositories,
+                showProgress = false
+            )
+        }
     }
 
 }
